beff987b807781fc58731c95931795c6035cb067
Anold <141809292+arnold-1324@users.noreply.github.com>
2024-08-11 13:52:56 +0530

    Update user.model.js

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,68 +1,83 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-username:{
-    type:String,
-    required:true,
-    unique:true,
-},
-fullName:{
-    type:String,
-    required:true,
-},
-password:{
-    type:String,
-    required:true,
-    minLength:6,
-},
-lastLogin:{
-    type:Date,
-    default:Date.now
-},
-isVerfied:{
-    type:Boolean,
-    default:false
-},
-resetPasswordToken: String,
-resetPasswordExpires: Date,
-verificationToken: String,
-verificationTokenExpiresAt: Date,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
 
-email:{
-    type:String,
-    required:true,
-    unique:true,
-},
-followers:[
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        default:[]
-    }
-],
-following:[
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        default:[]
-    }
-],
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 
-profileImg:{
-    type:String,
-    default:"",
-},
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: [],
+        }
+    ],
 
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: [],
+        }
+    ],
 
-bio:{
-    type:String,
-    default:"",
-},
+    profileImg: {
+        type: String,
+        default: "",
+    },
 
+    bio: {
+        type: String,
+        default: "",
+    },
 
-},{timestamps:true})
+    accountPrivacy: {
+        type: String,
+        enum: ["public", "private"],
+        default: "public",
+    },
 
+    isAccountFrozen: {
+        type: Boolean,
+        default: false,
+    },
 
-const User=mongoose.model("User",userSchema);
+    accountFreezeUntil: {
+        type: Date,
+        default: null,
+    },
+
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
