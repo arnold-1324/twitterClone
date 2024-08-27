d85355c5b1885cceacc6304d18e8f6d45f4f6840
Anold <141809292+arnold-1324@users.noreply.github.com>
2024-08-27 11:09:19 +0000

    updated Backend and Frontend

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -1,14 +1,14 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-import Message from "../models/messageModel.js";
-import Conversation from "../models/conversationModel.js";
+import Message from "../models/message.model.js";
+import Conversation from "../models/conversation.model.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5000",
+        origin: "http://127.0.0.1:5000",
         methods: ["GET", "POST"],
     },
 });
