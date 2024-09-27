95272a20c4b031e29df3f9a956600747690fa4a9
Arnold Aron <anlsktl1318@gmail.com>
2024-09-27 12:31:24 +0000

    Unit Test  encrypt and decrypt msg

--- Backend/lib/utils/Msg_encryption/encrypt.js
+++ Backend/lib/utils/Msg_encryption/encrypt.js
@@ -7,9 +7,12 @@ const algorithm = 'aes-256-cbc';
 const secretKey = process.env.ENCRYPTION_SECRET; 
 const iv = crypto.randomBytes(16); 
 
-
+function generateKey(key) {
+    return crypto.createHash('sha256').update(key).digest('base64').substr(0, 32); 
+}
+const key = generateKey(secretKey);
 export const encrypt = (text) => {
-    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv);
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
     let encrypted = cipher.update(text, 'utf-8', 'hex');
     encrypted += cipher.final('hex');
     return { iv: iv.toString('hex'), encryptedData: encrypted };
@@ -17,7 +20,7 @@ export const encrypt = (text) => {
 
 
 export const decrypt = (encryption) => {
-    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), Buffer.from(encryption.iv, 'hex'));
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.from(encryption.iv, 'hex'));
     let decrypted = decipher.update(encryption.encryptedData, 'hex', 'utf-8');
     decrypted += decipher.final('utf-8');
     return decrypted;
