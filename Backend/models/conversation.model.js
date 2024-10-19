a9775ee8406c4a9a43c8fe6f8cf6effc9756730d
arnold-1324 <anlsktl1318@gmail.com>
2024-10-20 01:59:06 +0530

    converstion

--- Backend/models/conversation.model.js
+++ Backend/models/conversation.model.js
@@ -1,24 +1,21 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema(
-	{
-		participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  {
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }],
     lastMessage: {
-	text: String,
-	iv: String,
-	sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-	seen: {
-		type: Boolean,
-		default: false,
-	},
-},
-},
-{ timestamps: true }
+      text: String,
+      iv: String,
+      sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      seen: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  },
+  { timestamps: true }
 );
 
 const Conversation = mongoose.model("Conversation", conversationSchema);
 
 export default Conversation;
-
-
-
