a84362c3ff160778bb61939daddaef08556ed183
Arnold Aron <anlsktl1318@gmail.com>
2025-08-07 10:20:57 +0000

    pre-deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://twitterclone-backend-681i.onrender.com",
 				changeOrigin: true,
 				secure: false,
 			},
