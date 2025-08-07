3c15cf517ea640d7d30c40d9b816dad0f33207da
Arnold <141809292+arnold-1324@users.noreply.github.com>
2025-08-07 23:32:36 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -33,12 +33,12 @@ const PORT=process.env.PORT || 5000;
 app.use(compression());
 
 const __dirname = path.resolve();
-const limiter = rateLimit({
-    windowMs: 3 * 60 * 1000, 
-    max: 100, 
-    message: "Too many requests from this IP, please try again later."
-});
-app.use(limiter);
+//const limiter = rateLimit({
+//    windowMs: 3 * 60 * 1000, 
+//    max: 100, 
+ //   message: "Too many requests from this IP, please try again later."
+//});
+//app.use(limiter);
 
 //app.use(helmet()); 
 //app.use(express.static(path.join(__dirname, '../Frontend/dist')));
