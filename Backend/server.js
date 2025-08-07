d4b3a02444a1dee582df6d76e716dcb4f703303f
Arnold <141809292+arnold-1324@users.noreply.github.com>
2025-08-07 23:21:09 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use(compression());
 
 const __dirname = path.resolve();
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, 
+    windowMs: 3 * 60 * 1000, 
     max: 100, 
     message: "Too many requests from this IP, please try again later."
 });
